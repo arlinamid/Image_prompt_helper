c410fc0fd5dc8a0d4e5461339bb3c1e566742804
arlinamid <arlinamid@gmail.com>
2024-04-03 09:19:57 +0200

    First release v1.0.2

---
+++ assets/content-script-loader.index.jsx.07c26ee2.33581a38.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  (async () => {
+    await import(
+      /* @vite-ignore */
+      chrome.runtime.getURL("assets/index.jsx.07c26ee2.js")
+    );
+  })().catch(console.error);
+
+})();
